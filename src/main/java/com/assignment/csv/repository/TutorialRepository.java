ce9fafa05e28069a5d301ee562221d1b612fb76f
Ravi <ravi@vin.net>
2020-06-09 01:36:22 +0530

    Last Touch Point

--- src/main/java/com/assignment/csv/repository/TutorialRepository.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.csv.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assignment.csv.model.Tutorial;
-
-public interface TutorialRepository extends JpaRepository<Tutorial, Long>{
-}
